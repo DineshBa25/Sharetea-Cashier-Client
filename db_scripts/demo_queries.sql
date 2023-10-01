77404aa1cf90e478b427b38004aafee59960a3ef
nicholasdienstbier <nicholasdienstbier@gmail.com>
2023-10-01 17:08:26 -0500

    edited order of code, added functionality calls to very end

--- db_scripts/demo_queries.sql
+++ db_scripts/demo_queries.sql
@@ -1,28 +1,20 @@
-CREATE TABLE IF NOT EXISTS Employee(
-  employeeID INT PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS Employees(
+  employeeID SERIAL PRIMARY KEY,
   isManager BOOLEAN,
   firstName VARCHAR(20),
   lastName VARCHAR(20)
 );
 
-CREATE TABLE IF NOT EXISTS Ingredient(
-  ingredientID INT PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS Ingredients(
+  ingredientID SERIAL PRIMARY KEY,
   ingredientName VARCHAR(50),
   quantity INT,
   cost FLOAT
 );
 
-CREATE TABLE IF NOT EXISTS Product(
-  productID INT PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS Products(
+  productID SERIAL PRIMARY KEY,
   ingredientIDs VARCHAR(50)[], -- FK with ingredientIDs
-  price FLOAT
-);
-
-CREATE TABLE IF NOT EXISTS Order(
-  orderID INT PRIMARY KEY,
-  cashier VARCHAR(50), -- employeeID of employee
-  transactionID DATETIME,
-  products VARCHAR(50)[], -- FK with productIDs
   price FLOAT
 );
 
@@ -34,6 +26,28 @@ CREATE TABLE IF NOT EXISTS Finances(
   orders VARCHAR(50)[] -- FK with orderIDs
 );
 
+CREATE TABLE IF NOT EXISTS Orders(
+  orderID SERIAL PRIMARY KEY,
+  cashier VARCHAR(50), -- employeeID of employee
+  transactionTime TIMESTAMP,
+  price FLOAT
+);
+
+CREATE TABLE IF NOT EXISTS OrderProducts(
+  orderID INT,
+  productID VARCHAR(50),
+  PRIMARY KEY (orderID, productID)
+);
+
+ALTER TABLE OrderProducts ADD CONSTRAINT fk_order 
+  FOREIGN KEY (orderID) REFERENCES Orders(orderID);
+
+-- Assuming you have a Products table:
+ALTER TABLE OrderProducts ADD CONSTRAINT fk_product 
+  FOREIGN KEY (productID) REFERENCES Products(productID);
+
+
+-- MISC Queries, to show functionality:
 -- list in stock ingredients 
 SELECT ingredientName
 FROM Ingredient
