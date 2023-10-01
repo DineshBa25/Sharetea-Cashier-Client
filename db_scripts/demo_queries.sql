a4450add24b2f03bb2153d91a3e1531724c3c0f4
mohsin <87213341+mohsinkhan25@users.noreply.github.com>
2023-10-01 16:06:57 -0500

    Wrote 5 sql queries that instantiate the tables

--- db_scripts/demo_queries.sql
+++ db_scripts/demo_queries.sql
@@ -4,3 +4,32 @@ CREATE TABLE IF NOT EXISTS Employee(
   firstName VARCHAR(20),
   lastName VARCHAR(20)
 );
+
+CREATE TABLE IF NOT EXISTS Ingredient(
+  ingredientID INT PRIMARY KEY,
+  ingredientName VARCHAR(50),
+  quantity INT,
+  cost FLOAT
+);
+
+CREATE TABLE IF NOT EXISTS Product(
+  productID INT PRIMARY KEY,
+  ingredientIDs VARCHAR(50)[], -- FK with ingredientIDs
+  price FLOAT
+);
+
+CREATE TABLE IF NOT EXISTS Order(
+  orderID INT PRIMARY KEY,
+  cashier VARCHAR(50), -- employeeID of employee
+  transactionID DATETIME,
+  products VARCHAR(50)[], -- FK with productIDs
+  price FLOAT
+);
+
+CREATE TABLE IF NOT EXISTS Finances(
+  reportDate DATE PRIMARY KEY,
+  revenue FLOAT,
+  profit FLOAT,
+  expenses FLOAT,
+  orders VARCHAR(50)[] -- FK with orderIDs
+);
