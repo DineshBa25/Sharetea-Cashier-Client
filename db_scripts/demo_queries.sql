2728c7637e625aa6baa87eb8f8fbf4a5db62cad3
Dinesh Balakrishnan <90096608+DineshBa25@users.noreply.github.com>
2023-10-01 15:45:01 -0500

    Update demo_queries.sql

--- db_scripts/demo_queries.sql
+++ db_scripts/demo_queries.sql
@@ -1,14 +1,7 @@
-<<<<<<< HEAD
--- Nicks edits, testing
-
--- see most profitable day
-
--- list low ingredients 
-=======
 CREATE TABLE IF NOT EXISTS Employee(
   employeeID INT PRIMARY KEY,
   isManager BOOLEAN,
   firstName VARCHAR(20),
   lastName VARCHAR(20)
 );
->>>>>>> main
+
