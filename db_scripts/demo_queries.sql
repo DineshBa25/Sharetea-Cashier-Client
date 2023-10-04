84b13d2d17218405143d8396342c1b04fe257c47
nicholasdienstbier <nicholasdienstbier@gmail.com>
2023-10-03 21:45:32 -0500

    Finished demo_queries.sql file, now have 15 queries

--- db_scripts/demo_queries.sql
+++ db_scripts/demo_queries.sql
@@ -14,6 +14,7 @@ CREATE TABLE IF NOT EXISTS Ingredients(
 
 CREATE TABLE IF NOT EXISTS Products(
   productID SERIAL PRIMARY KEY,
+  productName VARCHAR(200),
   ingredientIDs VARCHAR(50)[], -- FK with ingredientIDs
   price FLOAT
 );
@@ -35,41 +36,126 @@ CREATE TABLE IF NOT EXISTS Orders(
 
 CREATE TABLE IF NOT EXISTS OrderProducts(
   orderID INT,
-  productID VARCHAR(50),
+  productID SERIAL,
   PRIMARY KEY (orderID, productID)
 );
 
 ALTER TABLE OrderProducts ADD CONSTRAINT fk_order 
   FOREIGN KEY (orderID) REFERENCES Orders(orderID);
 
--- Assuming you have a Products table:
 ALTER TABLE OrderProducts ADD CONSTRAINT fk_product 
   FOREIGN KEY (productID) REFERENCES Products(productID);
 
-
 -- MISC Queries, to show functionality:
--- list in stock ingredients 
-SELECT ingredientName
+-- list in stock ingredients - Query 1
+SELECT ingredientName, quantity
 FROM Ingredients
 WHERE quantity > 0;
 
--- list low stock ingredients
-SELECT ingredientName
+-- list low stock ingredients - Query 2
+SELECT ingredientName, quantity
 FROM Ingredients
 WHERE quantity < 10;
 
--- see most profitable 3 days
-SELECT reportDate, profit
-FROM Finances
-ORDER BY profit DESC
+-- sorting ingredients by price from high to low - Query 3
+SELECT *
+FROM ingredients
+ORDER BY price cost;
+
+-- see least profitable 3 days - Query 4
+SELECT
+    DATE(transactiontime) AS day,
+    SUM(price) AS total_revenue
+FROM orders
+GROUP BY day
+ORDER BY total_revenue ASC
 LIMIT 3;
 
--- see least profitable 3 days
-SELECT reportDate, profit,
-FROM Finances
-ORDER BY profit ASC
-LIMIT 3;
-
--- list Employees currently in database
+-- list Employees currently in database - Query 5
 SELECT firstName, lastName
 FROM Employees;
+
+-- list Employees last names sorted alphabetically - Query 6
+SELECT lastName
+FROM Employees
+ORDER BY lastName ASC;
+
+-- Show orders by a certain cashier on a certain day - Query 7
+SELECT
+    orderid,
+    transactiontime,
+    price
+FROM orders
+WHERE cashier = 'Nicholas'
+    AND DATE(transactiontime) = '2023-11-24';
+
+-- Calculate sales per week - Query 8
+SELECT 
+    DATE_TRUNC('week', transactiontime) AS week_start,
+    SUM(price) AS total_sales
+FROM 
+    orders
+GROUP BY 
+    week_start
+ORDER BY 
+    week_start;
+    
+-- Sort products alphabetically - Query 9
+SELECT *
+FROM products
+ORDER BY productname;
+
+-- Top 3 weeks in terms of order count - Query 10
+SELECT 
+    DATE_TRUNC('week', transactiontime) AS week_start,
+    COUNT(*) AS order_count
+FROM 
+    orders
+GROUP BY 
+    week_start
+ORDER BY 
+    order_count DESC
+LIMIT 3;
+
+-- sorting products by price from high to low - Query 11
+SELECT *
+FROM products
+ORDER BY price DESC;
+
+-- REQUIRED QUERIES
+-- 1. 52 Weeks of Sales History 
+SELECT 
+    DATE_TRUNC('week', transactiontime) AS week_start,
+    COUNT(*) AS order_count
+FROM 
+    orders
+GROUP BY 
+    week_start
+ORDER BY 
+    week_start;
+
+-- 2. Realistic Sales History
+SELECT
+    EXTRACT(HOUR FROM transactiontime) AS hour_of_day,
+    SUM(price) AS total_revenue
+FROM orders
+WHERE DATE(transactiontime) = '2023-10-03' -- the date to analyze
+GROUP BY hour_of_day
+ORDER BY hour_of_day;
+
+-- 3. 2 Peak Days
+SELECT
+    DATE(transactiontime) AS day,
+    SUM(price) AS total_revenue
+FROM orders
+GROUP BY day
+ORDER BY total_revenue DESC
+LIMIT 10;
+
+-- 4. 20 Items in Invetory
+SELECT COUNT(*) AS row_count
+FROM ingredients;
+
+
+GRANT ALL PRIVILEGES ON Employees, Ingredients, Products, Finances, Orders, OrderProducts TO csce331_970_mohsin, csce331_970_dineshb, csce331_970_ia601612
+, csce331_970_gpresent, csce331_970_nicholasdienstbier;
