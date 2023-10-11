8921f14e2e917f0005c306ca19ad0b2b0ac1013d
nicholasdienstbier <nicholasdienstbier@gmail.com>
2023-10-11 12:12:07 -0500

    Added rudimentary elements to both products-view and the productsController, work in progress currently no actual information is included. Just a skeleton of the final view.

--- src/main/java/com/goattechnologies/pos/ProductsController.java
+++ src/main/java/com/goattechnologies/pos/ProductsController.java
@@ -1,4 +1,28 @@
 package com.goattechnologies.pos;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TableView;
+
+import java.io.IOException;
+
 public class ProductsController {
+    @FXML
+    private ListView<String> menuListView;
+
+    @FXML
+    private ListView<CartItem> cartListView;
+
+    @FXML
+    public void initialize() {
+
+    }
+
+    public void handleBackButton(ActionEvent event) throws IOException {
+        Node node = FXMLLoader.load(getClass().getResource("manager-view.fxml"));
+        Main.getMainController().setView(node);
+    }
 }
