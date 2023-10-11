5783dd560160be22adc5e9403992ce3465c32a30
mohsinkhan25 <mohsin.ik23@gmail.com>
2023-10-10 20:45:55 -0500

    Delete src/main/java/com/goattechnologies/pos/dbSetup.java

--- src/main/java/com/goattechnologies/pos/dbSetup.java
+++
@@ -1,7 +0,0 @@
-package com.goattechnologies.pos;
-
-public class dbSetup {
-    public static final String user = "csce331_970_gpresent";
-    public static final String pswd = "gpresent";
-    // You can also add other constants like database URL, driver class, etc., if needed
-}
