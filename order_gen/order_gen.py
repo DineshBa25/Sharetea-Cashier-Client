8fdcec4c2fd866a68b58848379443427a08fefef
Cole Broberg <gpresent@tamu.edu>
2023-10-16 16:00:55 -0500

    updated orders csv with multiple items per order

--- order_gen/order_gen.py
+++ order_gen/order_gen.py
@@ -9,27 +9,26 @@ workers = [
 ]
 
 items = [
-    ["Classic Milk Tea", 5],
-    ["Honey Milk Tea", 5.50],
-    ["Classic Coffee", 3.50],
-    ["Coffee Milk Tea", 6],
-    ["Ginger Milk Tea", 5.50],
-    ["Classic Pearl Milk Tea", 6.50],
-    ["Hokkaido Pearl Milk Tea", 6.50],
-    ["Okinawa Pearl Milk Tea", 6.50],
-    ["Thai Pearl Milk Tea", 7],
-    ["Taro Pearl Milk Tea", 7],
-    ["Mango Green Milk Tea", 6.50],
-    ["Matcha Red Bean Milk Tea", 6.50],
+    ["Classic Milk Tea", 4],
+    ["Honey Milk Tea", 5],
+    ["Classic Coffee", 5.50],
+    ["Coffee Milk Tea", 5.50],
+    ["Classic Pearl Milk Tea", 4.50],
+    ["Hokkaido Pearl Milk Tea", 6],
+    ["Okinawa Pearl Milk Tea", 5.50],
+    ["Thai Pearl Milk Tea", 5],
+    ["Taro Pearl Milk Tea", 5],
+    ["Mango Green Milk Tea", 5],
+    ["Matcha Red Bean Milk Tea", 6],
     ["Mango Green Tea", 5.50],
-    ["Wintermelon Lemonade", 4],
-    ["Strawberry Tea", 5.50],
-    ["Tropical Fruit Tea", 5.50],
-    ["Fresh Milk Tea", 5.50],
-    ["Oreo Ice Blended with Pearl", 7.50],
-    ["Coffee Ice Blended with Ice Cream", 5.50],
-    ["Mango Ice Blended with Ice Cream", 5.50],
-    ["Passion Fruit Orange and Grapefruit Tea", 6],
+    ["Wintermelon Lemonade", 5],
+    ["Strawberry Tea", 5],
+    ["Tropical Fruit Tea", 6.5],
+    ["Fresh Milk Tea", 4.50],
+    ["Oreo Ice Blended with Pearl", 6],
+    ["Coffee Ice Blended with Ice Cream", 6.50],
+    ["Mango Ice Blended with Ice Cream", 7.50],
+    ["Passion Fruit Orange and Grapefruit Tea", 5.50],
 ]
 
 item_popularity = [
@@ -37,7 +36,6 @@ item_popularity = [
     10, # Honey Milk Tea
     7, # Classic Coffee
     11, # Coffee Milk Tea
-    9, # Ginger Milk Tea
     17, # Classic Pearl Milk Tea
     8, # Hokkaido Pearl Milk Tea
     7, # Okinawa Pearl Milk Tea
@@ -58,24 +56,22 @@ item_popularity = [
 item_popularity_total = sum(item_popularity)
 
 toppings = [
-    ["Pearl", 0.75],
+    ["Pearl", 0.5],
     ["Mini Pearl", 0.75],
     ["Ice Cream", 1],
-    ["Pudding", 1],
+    ["Pudding", 0.75],
     ["Aloe Vera", 0.75],
-    ["Red Bean", 0.75],
-    ["Herb Jelly", 0.75],
+    ["Herb Jelly", 0.5],
     ["Aiyu Jelly", 0.75],
     ["Lychee Jelly", 0.75],
 ]
 
 topping_popularity = [
     5, # Pearl
-    1, # Mini Pearl
+    3, # Mini Pearl
     1, # Ice Cream
     1, # Pudding
     1, # Aloe Vera
-    1, # Red Bean
     1, # Herb Jelly
     1, # Aiyu Jelly
     1, # Lychee Jelly
@@ -104,8 +100,22 @@ day_popularity = [
 day_popularity_total = sum(day_popularity)
 
 topping_chance = 0.4
+additional_item_chance = 0.1
 order_id = 0
-# Order format: [id, timestamp, [item] (name, price), [[toppings (names)], price], worker (name)]
+# Order format: 
+# [
+#   id, 
+#   timestamp, 
+#   [
+    #   [item names], 
+    #   ..., 
+    #   items price,    ],
+#   [
+    #   [toppings (names)], 
+    #   [toppings2 (names)],
+    #   ... 
+    #   toppings price  ], 
+#   worker (name)   ]
 def generate_order():
     global order_id
     order = [order_id]
@@ -113,48 +123,76 @@ def generate_order():
     minutes = str(random.randint(0, 59))
     timestamp = str(random.randint(11, 22)) + ":" + minutes.zfill(2)
     order.append(timestamp)
-    ordernum = random.randint(1, item_popularity_total)
     workernum = random.randint(0, len(workers) - 1)
-    for i in range(len(item_popularity)):
-        ordernum -= item_popularity[i]
-        if ordernum <= 0:
-            order.append(items[i])
-            break
-    # Toppings are optional, so we need to check if we should add one or not
-    # Create temporary copies of the toppings and their popularity
-    top = toppings.copy()
-    pop = topping_popularity.copy()
-    pop_tot = sum(pop)
-    top_chance = topping_chance
-    order.append([[]])
+    
+    num_items = 0
+    item_tot = 0
     top_tot = 0
-    while random.random() < top_chance and len(top) > 0:
-        toppingnum = random.randint(1, pop_tot)
-        for i in range(len(pop)):
-            toppingnum -= pop[i]
-            if toppingnum <= 0:
-                order[3][0].append(top[i][0])
-                top_tot += top[i][1]
-                top.pop(i)
-                pop.pop(i)
+    while True:
+        ordernum = random.randint(1, item_popularity_total)
+        for i in range(len(item_popularity)):
+            ordernum -= item_popularity[i]
+            if ordernum <= 0:
+                if num_items == 0:
+                    order.append([[]])
+                    order[2][0].append(items[i][0])
+                else:
+                    order[2][0].append(items[i][0])
+                item_tot += items[i][1]
                 break
+        # Toppings are optional, so we need to check if we should add one or not
+        # Create temporary copies of the toppings and their popularity
+        top = toppings.copy()
+        pop = topping_popularity.copy()
         pop_tot = sum(pop)
-        top_chance *= 0.75
-    order[3][0].sort()
+        top_chance = topping_chance
+        if num_items == 0:
+            order.append([[]])
+        else:
+            order[3].append([])
+        while random.random() < top_chance and len(top) > 0:
+            toppingnum = random.randint(1, pop_tot)
+            for i in range(len(pop)):
+                toppingnum -= pop[i]
+                if toppingnum <= 0:
+                    order[3][num_items].append(top[i][0])
+                    top_tot += top[i][1]
+                    top.pop(i)
+                    pop.pop(i)
+                    break
+            pop_tot = sum(pop)
+            top_chance *= 0.75
+        
+        order[3][num_items].sort()
+        if len(order[3][num_items]) == 0:
+            order[3][num_items].append("None")
+        num_items += 1
+        if random.random() > 0.1:
+            break
+    order[2].append(item_tot)
     order[3].append(top_tot)
-    if len(order[3][0]) == 0:
-        order[3][0].append("None")
     order.append(workers[workernum])
     return order
 
 def order_string(order):
-    string = str(order[0]) + ", " + order[1] + ", " + order[2][0] + ", " + str(order[2][1]) + ", "
-    for i in range(0, len(order[3][0])):
-        if i == len(order[3][0]) - 1:
-            string += order[3][0][i] + ", "
+    string = str(order[0]) + ", " + order[1] + ", "
+    
+    for i in range(0, len(order[2][0])):
+        if i == len(order[2][0]) - 1:
+            string += order[2][0][i] + ", "
         else:
-            string += order[3][0][i] + "; "
-    string += str(order[3][1]) + ", " + order[4]
+            string += order[2][0][i] + "; "
+    string += str(order[2][1]) + ", "
+    for i in range(0, len(order[3])):
+        if i < len(order[3]) - 1:
+            for j in range(0, len(order[3][i])):
+                if (i == len(order[3]) - 2 and j == len(order[3][i]) - 1):
+                    string += order[3][i][j] + ", "
+                else:
+                    string += order[3][i][j] + "; "
+    string += str(order[3][-1])
+    string += ", " + order[4]
+    
     return string
 
 weekly_total = 19300
@@ -171,8 +209,8 @@ def generate_day(week, weekday):
     while daily_earnings < target:
         order = generate_order()
         orders.append(order)
-        daily_earnings += order[2][1]
-        daily_earnings += order[3][1]
+        daily_earnings += order[2][-1]
+        daily_earnings += order[3][-1]
     day.append(daily_earnings)
     return day
 
@@ -193,7 +231,7 @@ if __name__ == "__main__":
     for i in range(1, 53):
         year.append(generate_week(i))
     results = open("orders.csv", "w")
-    results.write("Week, Day, Order ID, Timestamp, Item, Item Price, Topping(s), Topping Price, Worker\n")
+    results.write("Week, Day, Order ID, Timestamp, Item(s), Items' Price, Topping(s), Topping Price, Worker\n")
     for week in year:
         for day in week:
             for order in day[2]:
