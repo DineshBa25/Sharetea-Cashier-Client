5409b00284352edee9b8ea5525222c772580e237
Ilham Aryawan <ia601612@tamu.edu>
2023-10-17 19:15:05 -0500

    Added JavaDocs to Login2Controller

--- src/main/java/com/goattechnologies/pos/Login2Controller.java
+++ src/main/java/com/goattechnologies/pos/Login2Controller.java
@@ -10,11 +10,21 @@ import javafx.scene.text.Text;
 import java.io.IOException;
 import java.util.Objects;
 
+/**
+ * The `Login2Controller` class is responsible for handling user interactions and logic
+ * on the second screen of the login process. It welcomes the user and provides options
+ * to start a new order or go back to the previous screen.
+ *
+ * @author Dinesh Balakrishnan, Ilham Aryawan, Moshin Khan
+ */
 public class Login2Controller {
 
     @FXML
     private Text name;
 
+    /**
+     * Initializes the Login2Controller. Sets the font and displays a welcome message with the user's name.
+     */
     public void initialize() {
         Font customFont = Font.font("Arial", 20);
         name.setFont(customFont);
@@ -23,6 +33,12 @@ public class Login2Controller {
         name.setText("Welcome, " + employeeName + "!");
     }
 
+    /**
+     * Handles the event when the user clicks the "Start New Order" button.
+     * Clears the cart and navigates the user to the menu screen to start a new order.
+     *
+     * @throws IOException if there is an error while navigating to the menu screen.
+     */
     @FXML
     protected void onLogin1Click() throws IOException {
         //empty cart before starting a new order
@@ -32,7 +48,13 @@ public class Login2Controller {
         Main.getMainController().setView(node);
     }
 
-
+    /**
+     * Handles the event when the user clicks the "Back" button.
+     * Navigates the user back to the previous login screen.
+     *
+     * @param event The event that triggered the action.
+     * @throws IOException if there is an error while navigating back to the previous screen.
+     */
     public void handleBackButton(ActionEvent event) throws IOException {
         Node node = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("login1-view.fxml")));
         Main.getMainController().setView(node);
